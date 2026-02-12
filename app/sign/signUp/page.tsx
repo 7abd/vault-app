59fa60973c2dabe298444304a9ab15c97e79f4f0
7abd <155910416+7abd@users.noreply.github.com>
2026-02-12 06:21:31 +0100

    move the types

--- app/sign/signUp/page.tsx
+++ app/sign/signUp/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { createClient } from "@/lib/supabase/SupabaseClient"
-import { useAuth } from "@/lib/supabase/context"
 
 
 export default function SignUp() {
