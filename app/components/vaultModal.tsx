3af317dfb013384a750d368cb0ffe55c73471c40
7abd <155910416+7abd@users.noreply.github.com>
2026-02-12 06:15:16 +0100

    adding handlesubmit and handleImage

--- app/components/vaultModal.tsx
+++ app/components/vaultModal.tsx
@@ -1,21 +1,75 @@
 import { useState, Dispatch, SetStateAction } from "react"
-
+import { encryptWithKey ,generateSalt ,deriveCryptoKey,fileToBase64} from "@/lib/crypto";
+import { createClient } from "@/lib/supabase/SupabaseClient";
+import { useAuth } from "@/lib/context";
 type Content = {
  title: string 
   secretMsg : string 
+  duration : number
+  type: 'note' | 'password' | 'image'
 }
 
 export default function VaultModal({ isOpen, setIsOpen }:
-   { isOpen: boolean; setIsOpen: Dispatch<SetStateAction<boolean>> }) {
-      const [content,setContent] = useState <Content> ({title:'' , secretMsg:''})
-
-      const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-      ) => {
+   { isOpen: boolean; setIsOpen: Dispatch<SetStateAction<boolean>>}) {
+      const [content,setContent] = useState <Content> ({title:'' , secretMsg:'' , duration:0,type:'note'})
+   const supabase = createClient()
+   const {user,fetchVaultItems} = useAuth();
+  
+      const handleChange =  (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         const { name, value } = e.target
         setContent((prev) => ({ ...prev, [name]: value }))
 
       }
-     
+ 
+      async function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const file = e.target.files?.[0]
+        if (!file) return
+      
+        
+        if (file.size > 2 * 1024 * 1024) {
+          alert("Image too large")
+          return
+        }
+      
+        const base64 = await fileToBase64(file)
+      
+        setContent(prev => ({
+          ...prev,
+          secretMsg: base64
+        }))
+      }
+      
+    const handleSubmit =  async (e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            const saltUint8 = generateSalt();
+            const salt = btoa(String.fromCharCode(...saltUint8));
+            const key = await deriveCryptoKey("myMasterPassword", salt);
+            const { encrypted_content, encryption_iv } = await encryptWithKey(content.secretMsg, key);
+        
+            if (!user) return;
+            const { data, error } = await supabase
+            .from("vault_items")
+            .insert({
+              user_id: user.id,
+              title: content.title,
+              type: content.type,
+              encrypted_content,
+              encryption_iv,
+              duration_minutes: content.duration,
+              next_unlock_at: new Date(Date.now() + content.duration * 60 * 1000),
+              frequency: "once",
+            })
+           
+          
+          if (error) {
+            console.error("Error inserting vault item:", error);
+          } else {
+            
+           await fetchVaultItems()
+          }
+          setIsOpen(false)
+          setContent({title:'' , secretMsg:'' , duration:0,type:'note'})
+    }
       console.log(content)
     return (
         isOpen && (
@@ -29,7 +83,8 @@ export default function VaultModal({ isOpen, setIsOpen }:
               <div className="relative bg-gray-900 border border-gray-800 p-8 rounded-2xl w-full max-w-md shadow-2xl">
                 <h2 className="text-xl font-bold mb-4">New Secret Capsule</h2>
                 
-                <div className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-4">
+
                   <input 
                   name="title"
                   onChange={handleChange}
@@ -38,6 +93,26 @@ export default function VaultModal({ isOpen, setIsOpen }:
                     placeholder="Title (e.g. My Ledger Key)" 
                     className="w-full bg-gray-800 p-3 rounded-lg border border-gray-700 outline-none focus:border-teal-400"
                   />
+                   <select
+                    name="type"
+                    value={content.type}
+                    onChange={handleChange}
+                    className="w-full bg-gray-800 p-3 rounded-lg border border-gray-700 outline-none focus:border-teal-400"
+                  >
+                    <option value="note">Note</option>
+                    <option value="password">Password</option>
+                    <option value="image">Image</option>
+                  </select>
+                  <input
+                      type="number"
+                      name="duration"
+                      min={1}
+                      value={content.duration}
+                      onChange={handleChange}
+                      placeholder="Lock duration (minutes)"
+                      className="w-full bg-gray-800 p-3 rounded-lg border border-gray-700 outline-none focus:border-teal-400"
+                        />
+          {content.type !== 'image' && 
                   <textarea 
                   name="secretMsg"
                   value={content.secretMsg}
@@ -46,7 +121,15 @@ export default function VaultModal({ isOpen, setIsOpen }:
                     placeholder="The Secret Content..." 
                     className="w-full bg-gray-800 p-3 rounded-lg border border-gray-700 h-32 outline-none focus:border-teal-400"
                   />
-                </div>
+              }
+          {content.type === "image" && (
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="w-full bg-gray-800 p-3 rounded-lg border border-gray-700"
+          />
+        )}
     
                 <div className="mt-6 flex gap-3">
                   <button 
@@ -55,13 +138,15 @@ export default function VaultModal({ isOpen, setIsOpen }:
                   >
                     Cancel
                   </button>
-                  <button className="flex-1 bg-teal-400 text-black py-3 rounded-xl font-bold">
+                  <button type="submit"  className="flex-1 bg-teal-400 text-black py-3 rounded-xl font-bold">
                     Lock & Save
                   </button>
                 </div>
+                </form>
               </div>
             </div>
           
     )
 )
 }
+
