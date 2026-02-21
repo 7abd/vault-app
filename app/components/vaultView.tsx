da7973de053c6eed588915356f768c1d9d058edd
7abd <155910416+7abd@users.noreply.github.com>
2026-02-21 23:51:21 +0000

    added handleDycrypt for and content state

--- app/components/vaultView.tsx
+++ app/components/vaultView.tsx
@@ -1,16 +1,29 @@
 'use client'
 import { useVaultCtx } from "@/lib/vaultContext"
 import { VaultEntry } from "@/lib/types";
-
+import { useState, useEffect } from "react";
+import { decryptWithKey } from "@/lib/crypto";
 
 export default function VaultView({ vaultItem, setVaultOpen }: { 
   vaultItem: VaultEntry | null; 
   setVaultOpen: (open: boolean) => void 
 }) {
-    const { error } = useVaultCtx()
-  
-   
-  
+  const [content, setContent] = useState<string | null>(null)
+  const { withDecrypted, error } = useVaultCtx()
+
+  useEffect(() => {
+    if (!vaultItem) return;
+    
+    const handleDecrypt = async () => {
+      const result = await withDecrypted(async (key) => {
+        return await decryptWithKey(vaultItem.encrypted_content, vaultItem.encryption_iv, key);
+      });
+      setContent(result)
+    };
+
+    handleDecrypt();
+  }, [vaultItem, withDecrypted]);
+
   return (
     <div className="max-w-xl mx-auto p-6 bg-gray-900 rounded-2xl border border-gray-800">
       <div className="flex justify-between items-center mb-6">
@@ -25,7 +38,7 @@ export default function VaultView({ vaultItem, setVaultOpen }: {
           <p className="text-red-400 text-sm">{error}</p>
         ) : (
           <p className="text-gray-300 font-mono break-all">
-             Decrypting...
+            {content || "Decrypting..."}
           </p>
         )}
       </div>
